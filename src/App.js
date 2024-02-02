8cbac19f5856a88a6e16953379f7817dcaaafa3c
fukai0116 <134059797+fukai0116@users.noreply.github.com>
2024-02-03 03:27:20 +0900

    コンテンツ追加

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Home from './Home';
 import Footer from './components/Footer';
 import EventDetails from './components/EventDetails';
 import No1 from './pages/No1'; 
-import No2 from './pages/No2'; 
+import No22 from './pages/No22'; 
 import No3 from './pages/No3'; 
 import No4 from './pages/No4'; 
-import No5 from './pages/No5'; 
+import No55 from './pages/No55'; 
 
 const App = () => {
   return (
@@ -18,10 +18,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/EventDetails" element={<EventDetails />} />
         <Route path="/topic-page-1" element={<No1 />} /> 
-        <Route path="/topic-page-2" element={<No2 />} /> 
+        <Route path="/topic-page-2" element={<No22 />} /> 
         <Route path="/topic-page-3" element={<No3 />} /> 
         <Route path="/topic-page-4" element={<No4 />} /> 
-        <Route path="/topic-page-5" element={<No5 />} /> 
+        <Route path="/topic-page-5" element={<No55 />} /> 
       </Routes>
       <Footer />
     </Router>
